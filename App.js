deae7d3ddeb86811f33a42ce3c6383ce911d22ed
Nuthan Hegde <nthnua@gmail.com>
2022-04-24 22:46:34 +0530

    Implement channels page

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { NativeBaseProvider } from 'native-base'
+import { StyleSheet, Text, View } from 'react-native'
+import Channels from './features/Channels/Channels'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NativeBaseProvider>
+      <StatusBar style='auto' />
+      <Channels />
+    </NativeBaseProvider >
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
--- features/firebase.js
+++ features/firebase.js
@@ -1,28 +1,41 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, onValue, set } from 'firebase/database';
+import { initializeApp } from 'firebase/app'
+import { addDoc, collection, getDoc, doc, getFirestore, query, where, getDocs } from 'firebase/firestore/lite'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAlOjk030KMmlLTwDUWTVD1vB-T_3o_Ij8",
-    authDomain: "rn-evns.firebaseapp.com",
-    projectId: "rn-evns",
-    storageBucket: "rn-evns.appspot.com",
-    messagingSenderId: "696181640992",
-    appId: "1:696181640992:web:f87d921e78aaf7168f1145"
-};
+  apiKey: 'AIzaSyAlOjk030KMmlLTwDUWTVD1vB-T_3o_Ij8',
+  authDomain: 'rn-evns.firebaseapp.com',
+  projectId: 'rn-evns',
+  storageBucket: 'rn-evns.appspot.com',
+  messagingSenderId: '696181640992',
+  appId: '1:696181640992:web:f87d921e78aaf7168f1145'
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
 
-export function addUser(userId, name, type, subscriptions) {
-    const db = getDatabase();
-    const reference = ref(db, 'users/' + userId);
-    set(reference, {
-        name,
-        type,
-        subscriptions
-    });
+export async function addUser (userId, name, type, subscriptions) {
+  try {
+    await addDoc(collection(getFirestore(), 'users', '1'), {
+      name,
+      type,
+      subscriptions,
+      timestamp: Date.now()
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
+export function getChannels (userId) {
+  const usersRef = collection(db, 'users')
+  const q = query(usersRef, where('id', '==', `${userId}`))
+  return getDocs(q).then(qs => {
+    const chnlRef = collection(db, 'channels')
+    const chnlQuery = query(chnlRef, where('id', 'in', qs.docs[0].get('subscriptions')))
+    return getDocs(chnlQuery)
+  }).catch(err => console.error(err))
 }
